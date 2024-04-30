5f00330a810d5224088317e4c9c58ffb35a7916e
Mahmuda Akter <mahmudapakter@gmail.com>
2024-05-01 02:33:03 +0600

    text editor added

--- js/deposit.js
+++ js/deposit.js
@@ -1,36 +1,3 @@
-/* 
-document.getElementById('btn-deposit').addEventListener('click', function () {
-       // getInputFieldById('deposit-input');
-
-       const deposit = document.getElementById('deposit-input');
-       const depositInputString = deposit.value;
-       const depositInput = parseFloat(depositInputString);
-
-       if (!isNaN(depositInputString)) {
-              const depositNow = document.getElementById('deposit-total');
-              const depositValueString = depositNow.innerText;
-              const depositValue = parseFloat(depositValueString);
-
-              const depositTotal = depositInput + depositValue;
-              depositNow.innerText = depositTotal;
-
-              deposit.value = '';
-
-              const balanceNow = document.getElementById('balance-total');
-              const balanceValueString = balanceNow.innerText;
-              const balanceValue = parseFloat(balanceValueString);
-
-
-              const balanceUpdate = balanceValue + depositInput;
-              balanceNow.innerText = balanceUpdate;
-       } else {
-              alert('Please give us a number');
-              withdraw.value = '';
-
-       }
-
-})  */
-
 document.getElementById('btn-deposit').addEventListener('click', function () {
        const newDepositAmount = getInputFieldById('deposit-input');
 
